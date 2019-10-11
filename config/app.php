9c537536d4eeb34203f05ab11a8b463f765d0916
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2019-10-11 16:41:26 +0200

    mumify html css

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'HTMLMin' => HTMLMin\HTMLMin\Facades\HTMLMin::class,
 
     /*
     |--------------------------------------------------------------------------
