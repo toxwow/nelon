e696d9ec2172f298baa0bc1e39717b7d4ce9921a
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2019-10-26 22:40:14 +0200

    fix js ssl

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,6 @@
 
         gtag('config', 'UA-149773831-1');
     </script>
-    <script type="text/javascript"> //<![CDATA[
-        var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/");
-        document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
-        //]]></script>
-    <script language="JavaScript" type="text/javascript">
-        TrustLogo("https://www.positivessl.com/images/seals/positivessl_trust_seal_sm_124x32.png", "POSDV", "none");
-    </script>
     @stack('description')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
